230b0aea5b31f50c2acabc527c3d9261808d8a03
Leo <leonardopatino99@gmail.com>
2022-11-29 12:06:52 -0500

    carpeta api excluida para build

--- services/postsService.js
+++ services/postsService.js
@@ -1,16 +1,18 @@
 export const getPosts = async ()=>{
-  const resp = await fetch('http://localhost:3000/api/posts',{
-    method: 'POST',
-    body: 'all',
-  });
+  // const resp = await fetch('http://localhost:3000/api/posts',{
+  //   method: 'POST',
+  //   body: 'all',
+  // });
+  const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await resp.json();
   return data;
 }
 export const getPostOne = async (id)=>{
-  const res = await fetch('http://localhost:3000/api/posts',{
-    method: 'POST',
-    body: `${id}`,
-  });
+  // const res = await fetch('http://localhost:3000/api/posts',{
+  //   method: 'POST',
+  //   body: `${id}`,
+  // });
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
   const data = await res.json();
 
   return data;
